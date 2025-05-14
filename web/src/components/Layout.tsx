71e460c3229dd0e07c67e48c7f8d576b3adcd2de
Jacob Roman <jcbrmn06@gmail.com>
2025-05-14 15:16:48 -0400

    change the logic so it works correctly

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -5,11 +5,7 @@ import Footer from './Footer';
 
 export default function Layout() {
     const flagEnabled = useFeatureFlagEnabled('test-flag');
-  console.log("flagEnabled1", flagEnabled);
-    console.log("LOCATIONB1", window.location.origin)
-    console.log("LOCAL1", import.meta.env.VITE_FEATURE_LAUNCH === 'true');
-    if ((flagEnabled && window.location.origin === 'https://forge-5v1.pages.dev') || import.meta.env.VITE_FEATURE_LAUNCH === 'false' ) {
-        console.log('this runs');
+    if ((flagEnabled && window.location.origin === 'https://forge-5v1.pages.dev') || import.meta.env.VITE_FEATURE_LAUNCH === 'true' ) {
         return (
             <div className="font-lexend bg-[url('/web-background.png')] bg-cover relative overflow-x-hidden">
                 <header className="bg-white">
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -4,12 +4,8 @@ import HomePage from "../components/HomePage";
 
 export default function Index() {
     const flagEnabled = useFeatureFlagEnabled('test-flag');
-    console.log("flagEnabled", flagEnabled);
-    console.log("LOCATIONB", window.location.origin)
-    console.log("LOCAL", import.meta.env.VITE_FEATURE_LAUNCH === 'true');
 
     if (flagEnabled && window.location.origin === 'https://forge-5v1.pages.dev' || import.meta.env.VITE_FEATURE_LAUNCH === 'true' ) {
-        console.log('THIS RENDERED')
         return <HomePage />
     }
 
