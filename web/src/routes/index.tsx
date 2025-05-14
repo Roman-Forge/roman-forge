cc0673323c4bb28945694323d11e7b0b615bd2e0
Jacob Roman <jcbrmn06@gmail.com>
2025-05-14 14:07:16 -0400

    added logs

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -4,6 +4,8 @@ import HomePage from "../components/HomePage";
 
 export default function Index() {
     const flagEnabled = useFeatureFlagEnabled('test-flag');
+    console.log("flagEnabled", flagEnabled);
+    console.log("LOCATIONB", window.location.origin)
 
     if (flagEnabled && window.location.origin === 'https://forge-5v1.pages.dev' || import.meta.env.VITE_FEATURE_LAUNCH === 'true' ) {
         return <HomePage />
